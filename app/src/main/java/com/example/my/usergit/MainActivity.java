7f7c547e68c9598cc38071fb0786c2a32331f646
hu <296115608@qq.com>
2016-05-30 10:43:57 +0800

    1，first

--- app/src/main/java/com/example/my/usergit/MainActivity.java
+++ app/src/main/java/com/example/my/usergit/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("这是第一次的第一行代码");
     }
 }
